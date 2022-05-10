8e9c32bcf3d13feb97b70a78146ba6e1469c7bbe
bridget hester <bridgethester@bridgets-MacBook-Air.local>
2022-05-09 22:47:56 -0500

    now has the ability to store information in browser

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,10 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "../header/Header";
 import TabBox from "../tabBox/TabBox";
 
 const Main = () => {
   const [collection, setCollection] = useState([]);
   const [wishlist, setWishlist] = useState([]);
+
+  useEffect(() => {
+    if (collection.length > 0)
+      localStorage.collection = JSON.stringify(collection);
+  }, [collection]);
+
+  useEffect(() => {
+    if (wishlist.length > 0) localStorage.wishlist = JSON.stringify(wishlist);
+  }, [wishlist]);
+
+  useEffect(() => {
+    if (localStorage.collection) {
+      setCollection(JSON.parse(localStorage.collection));
+    }
+    if (localStorage.wishlist) {
+      setWishlist(JSON.parse(localStorage.wishlist));
+    }
+  }, []);
+
   return (
     <div
       id="maindiv"
